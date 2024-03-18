bb06b9555c679884019bc1408357873814d229a8
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2024-03-18 22:09:11 +0800

    add hello_animation from hello_cg

---
+++ 2_bounce_animation/hello_animation.cpp
@@ -0,0 +1,55 @@
+﻿// hello_cg.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <gl/glut.h>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello World!\n";
+
+    glutInit(&argc, argv);
+    glutInitWindowSize(800, 600);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutCreateWindow("Simple");
+
+    glutDisplayFunc([]()
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glColor3f(1.0, 0, 0);
+            glRectf(-25, 25, 25,-25);
+            glFlush();
+        });
+
+    glutReshapeFunc([](int w, int h)
+        {
+            glViewport(0, 0, w, h);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+
+            auto aspectRatio = (float)w / (float)h;
+            if(w <= h)
+                glOrtho(-50, 50, -50 / aspectRatio, 50 / aspectRatio, -1, 1);
+            else
+                glOrtho(-50 * aspectRatio, 50 * aspectRatio, -50, 50, -1, 1);            
+
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+        });
+
+    glClearColor(0, 0, 1.0, 1);
+    glFlush();
+
+    glutMainLoop();
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
